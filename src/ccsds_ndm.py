7f623499a0827beb8c39bb20da944c7dd9c0e647
egemenimre <s.egemen.imre@gmail.com>
2020-11-25 00:16:29 +0100

    moved __init__.py contents to ccsds_ndm.py for flit to work

---
+++ src/ccsds_ndm.py
@@ -0,0 +1,9 @@
+# CCSDS-NDM: CCSDS Navigation Data Messages Read/Write Library
+#
+# Copyright (C) 2020 CCSDS-NDM Egemen Imre
+#
+# Licensed under GNU GPL v3.0. See LICENSE.rst for more info.
+"""
+CCSDS-NDM
+"""
+__version__ = "1.0"
