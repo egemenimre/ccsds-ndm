5814266785db4f120446a8e8c2e5b6d614acfdd9
egemenimre <s.egemen.imre@gmail.com>
2021-01-31 23:27:00 +0100

    Commented out testing

--- ccsds_ndm/tests/test_ndm_kvn_io.py
+++ ccsds_ndm/tests/test_ndm_kvn_io.py
@@ -36,35 +36,35 @@ kvn_xml_file_paths = {
     "NDMv2_strip": None,
 }
 
-kvn_file_paths = {
-    "AEMv2": Path("data", "kvn", "adm-testcase04a_multi.kvn"),
-    # "APMv2_2": Path("data", "kvn", "504x0b1c1_fig3_8_apm.kvn"),
-    # "CDMv2": Path("data", "kvn", "cdm_example_section4.kvn"),
-    # "OEMv2_1": Path("data", "kvn", "odmv2-testcase7a_xxx.kvn"),
-    # "OMMv2_1": Path("data", "kvn", "omm1_st.kvn"),
-    # "OMMv2_2": Path("data", "kvn", "omm1_ct.kvn"),
-    # "OPMv2_1": Path("data", "kvn", "502x0b2c1e2_fig3_2_opm.kvn"),
-    # "OPMv2_1": Path("data", "kvn", "502x0b2c1e2_fig3_4_opm.kvn"),
-    # "RDMv2": Path("data", "kvn", "508x1b1_figc_2_rdm.kvn"),
-    # "TDMv2": None,
-    # "NDMv2": None,
-    # "NDMv2_strip": None,
-}
-
-
-@pytest.mark.parametrize("ndm_key, path", kvn_file_paths.items())
-def test_read_file(ndm_key, path):
-    # *** read KVN files ***
-    working_dir = Path.cwd()
-
-    if path is not None:
-        kvn_path = process_paths(working_dir, path)
-
-        # read KVN file
-        ndm = NdmKvnIo().from_path(kvn_path)
-
-        # print(ndm)
-        print(NdmIo().to_string(ndm, NDMFileFormats.XML))
+# kvn_file_paths = {
+#     "AEMv2": Path("data", "kvn", "adm-testcase04a_multi.kvn"),
+#     # "APMv2_2": Path("data", "kvn", "504x0b1c1_fig3_8_apm.kvn"),
+#     # "CDMv2": Path("data", "kvn", "cdm_example_section4.kvn"),
+#     # "OEMv2_1": Path("data", "kvn", "odmv2-testcase7a_xxx.kvn"),
+#     # "OMMv2_1": Path("data", "kvn", "omm1_st.kvn"),
+#     # "OMMv2_2": Path("data", "kvn", "omm1_ct.kvn"),
+#     # "OPMv2_1": Path("data", "kvn", "502x0b2c1e2_fig3_2_opm.kvn"),
+#     # "OPMv2_1": Path("data", "kvn", "502x0b2c1e2_fig3_4_opm.kvn"),
+#     # "RDMv2": Path("data", "kvn", "508x1b1_figc_2_rdm.kvn"),
+#     # "TDMv2": None,
+#     # "NDMv2": None,
+#     # "NDMv2_strip": None,
+# }
+#
+#
+# @pytest.mark.parametrize("ndm_key, path", kvn_file_paths.items())
+# def test_read_file(ndm_key, path):
+#     # *** read KVN files ***
+#     working_dir = Path.cwd()
+#
+#     if path is not None:
+#         kvn_path = process_paths(working_dir, path)
+#
+#         # read KVN file
+#         ndm = NdmKvnIo().from_path(kvn_path)
+#
+#         # print(ndm)
+#         print(NdmIo().to_string(ndm, NDMFileFormats.XML))
 
 
 @pytest.mark.parametrize("ndm_key, path", kvn_xml_file_paths.items())
