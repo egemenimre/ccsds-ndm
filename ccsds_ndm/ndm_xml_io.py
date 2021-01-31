6f39d9d31321839eed9d166c1a14d41e04f001b9
egemenimre <s.egemen.imre@gmail.com>
2021-01-31 21:26:59 +0100

    Updated copyright

--- ccsds_ndm/ndm_xml_io.py
+++ ccsds_ndm/ndm_xml_io.py
@@ -1,6 +1,6 @@
 # CCSDS-NDM: CCSDS Navigation Data Messages Read/Write Library
 #
-# Copyright (C) 2021 CCSDS-NDM Project Team
+# Copyright (C) 2021 Egemen Imre
 #
 # Licensed under GNU GPL v3.0. See LICENSE.rst for more info.
 """
