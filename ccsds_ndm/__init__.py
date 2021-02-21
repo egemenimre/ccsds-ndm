ab9ef6fb5c9368c78eb89f80e45407f500301d8c
egemenimre <s.egemen.imre@gmail.com>
2021-02-21 22:59:34 +0100

    Updated (c) year

--- ccsds_ndm/__init__.py
+++ ccsds_ndm/__init__.py
@@ -1,6 +1,6 @@
 # CCSDS-NDM: CCSDS Navigation Data Messages Read/Write Library
 #
-# Copyright (C) 2020 CCSDS-NDM Egemen Imre
+# Copyright (C) 2021 CCSDS-NDM Egemen Imre
 #
 # Licensed under GNU GPL v3.0. See LICENSE.rst for more info.
 """
