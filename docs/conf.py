1989fd2e458e25b29134bfdb2caceac1a7d8f7b5
egemenimre <s.egemen.imre@gmail.com>
2026-03-15 22:08:54 +0100

    bunped the version

--- docs/conf.py
+++ docs/conf.py
@@ -24,9 +24,9 @@ author = "Egemen Imre"
 # Version Info
 # ------------
 # The short X.Y version.
-version = "3.0"
+version = "3.1"
 # The full version, including alpha/beta/rc tags.
-release = "3.0"
+release = "3.1"
 
 # -- General configuration ---------------------------------------------------
 # By default, highlight as Python 3.
